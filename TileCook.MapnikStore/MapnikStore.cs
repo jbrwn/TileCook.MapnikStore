f9540a5566613f9b75639cb455d0ba2c719a48aa
Joel Brown <joelsbrown1@gmail.com>
2015-08-04 12:05:49 -0700

    populate metadata in ctor

--- TileCook.MapnikStore/MapnikStore.cs
+++ TileCook.MapnikStore/MapnikStore.cs
@@ -8,6 +8,7 @@ using TileCook;
 using TileProj;
 using System.IO;
 using System.IO.Compression;
+using Newtonsoft.Json.Linq;
 
 namespace TileCook.MapnikStore
 {
@@ -15,13 +16,16 @@ namespace TileCook.MapnikStore
     {
         private Map m { get; set;}
 
-        public MapnikStore(string resource)
+        public MapnikStore(string resource, string id = null)
         {
             Mapnik.RegisterDefaultInputPlugins();
             m = new Map(256, 256);
             m.BufferSize = 256;
             m.Load(resource);
 
+            Id = id == null ? Path.GetFileNameWithoutExtension(resource) : id;
+            Name = m.Parameters.ContainsKey("name") ? (string)m.Parameters["name"] : "";
+            Description = m.Parameters.ContainsKey("description") ? (string)m.Parameters["description"] : "";
             MinZoom = m.Parameters.ContainsKey("minzoom") ? Convert.ToInt32(m.Parameters["minzoom"]) : 0;
             MaxZoom = m.Parameters.ContainsKey("maxzoom") ? Convert.ToInt32(m.Parameters["maxzoom"]) : 14;
             if (m.Parameters.ContainsKey("bounds"))
@@ -38,6 +42,17 @@ namespace TileCook.MapnikStore
             {
                 Bounds = new Envelope(-180, -90, 180, 90);
             }
+
+            if (m.Parameters.ContainsKey("json"))
+            {
+                JObject json = JObject.Parse((string)m.Parameters["json"]);
+                JToken vector_layers;
+                if (json.TryGetValue("vector_layers", out vector_layers))
+                {
+                    VectorLayers = vector_layers.ToObject<IEnumerable<VectorLayer>>();
+                } 
+            }
+
         }
 
         public VectorTile GetTile(ICoord coord)
@@ -62,8 +77,12 @@ namespace TileCook.MapnikStore
             }
         }
 
+        public string Id { get; private set; }
+        public string Name { get; private set; }
+        public string Description { get; private set; }
         public int MinZoom { get; private set; }
         public int MaxZoom { get; private set; }
         public IEnvelope Bounds { get; private set; }
+        public IEnumerable<VectorLayer> VectorLayers { get; private set; }
     }
 }
